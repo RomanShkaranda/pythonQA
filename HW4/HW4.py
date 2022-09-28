9d2de3016467da116cdc4115a012d26268f1dddd
RomanShakranda <mccarthyroman@gmail.com>
2022-09-28 18:26:03 +0300

    hw4 edited

--- HW4/HW4.py
+++ HW4/HW4.py
@@ -16,14 +16,16 @@ for listItem in lst1:
 print(lst2)
 
 # 2
-list1 = [11, 77, 4, 22, 0, 56, 5, 95, 7, 5, 87, 13, 45, 67, 44]
-list2 = []
+lst = [11, 77, 4, 22, 0, 56, 5, 95, 7, 5, 87, 13, 45, 67, 44]
 
-for listitem1 in list1:
-    if 74 > listitem1 > 21:
-        list2.append(listitem1)
+for i in lst:
+    if i < 21:
+        lst.pop(lst.index(i))
+for a in lst:
+    if a > 74:
+        lst.pop(lst.index(a))
 
-print(list2)
+print(lst)
 
 # 3
 text = "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
