d5d688ea206b991eaba581d9f5f682dbfa01a872
RomanShakranda <mccarthyroman@gmail.com>
2022-09-29 13:47:26 +0300

    hw4 edited

--- HW4/HW4.py
+++ HW4/HW4.py
@@ -10,33 +10,29 @@
 lst1 = ['1', '2', 3, True, 'False', 5, '6', 7, 8, 'Python', 9, 0, 'Lorem Ipsum']
 lst2 = []
 
-for listItem in lst1:
-    if type(listItem) == str:
-        lst2.append(listItem)
+for listitem in lst1:
+    if type(listitem) == str:
+        lst2.append(listitem)
 
 print(lst2)
 
 # 2
 lst = [11, 77, 4, 22, 0, 56, 5, 95, 7, 5, 87, 13, 45, 67, 44]
+lst2 = []
 
 for i in lst:
-    if i < 21:
-        lst.pop(lst.index(i))
-for a in lst:
-    if a > 74:
-        lst.pop(lst.index(a))
-
-print(lst)
+    if 74 > i > 21:
+        lst2.append(i)
+print(lst2)
 
 # 3
-text = "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
+text = "wooO But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
 splited = text.split()
 res = []
 
 for i in splited:
-    if i.endswith("o"):
+    if i.endswith("o") or i.endswith(("o").upper()):
         res.append(i)
-
 print(len(res))
 
 
