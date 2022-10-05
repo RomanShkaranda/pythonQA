8f79c9d2cb32c66fd348bfb8b08e833c4f4dc3d6
RomanShakranda <mccarthyroman@gmail.com>
2022-10-05 13:39:05 +0300

    hw6 added

---
+++ 6. Q&A mentor/HW6.1.py
@@ -0,0 +1,20 @@
+# завдання 1
+#
+# урл http://api.open-notify.org/astros.json
+# вивести список всіх астронавтів, що перебувають в даний момент на орбіті (дані не фейкові, оновлюються в режимі реального часу)
+
+
+import requests
+astr_list = []
+
+url = "http://api.open-notify.org/astros.json"
+response = requests.get(url)
+response_json = response.json()
+
+for astr in response_json["people"]:
+    astr_list.append(astr["name"])
+
+print(astr_list)
+
+
+
